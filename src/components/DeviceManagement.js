7253747613c6068b3745e299602955b3ec55ca99
KasidisT4N <kasidis.aia@student.mahidol.edu>
2025-03-23 03:12:53 +0700

    update

--- src/components/DeviceManagement.js
+++ src/components/DeviceManagement.js
@@ -5,6 +5,7 @@ import { db } from '../firebase';
 import { collection, getDocs, query, where, updateDoc, doc} from 'firebase/firestore';
 import { FiSettings } from 'react-icons/fi';
 import AddDevice from './AddDevice';
+import { ArrowUpward, ArrowDownward } from '@mui/icons-material';
 
 const Container = styled.div`
   padding: 20px;
@@ -153,6 +154,72 @@ const SoundIcon = styled.span`
     transform: scale(1.1);
   }
 `;
+const FiltersWrapper = styled.div`
+  display: flex;
+  justify-content: center;  
+  align-items: center;
+  gap: 20px;
+  margin-bottom: 10px;
+  flex-wrap: nowrap; /* Prevents wrapping */
+`;
+
+const FilterContainer = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 8px;
+  white-space: nowrap; /* Ensures text stays on one line */
+`;
+
+const FilterSelect = styled.select`
+  margin-top: 15px;
+  padding: 8px 8px;
+  font-size: 14px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  background-color: #333333;
+  color: #ffffff;
+  cursor: pointer;
+  transition: all 0.3s ease-in-out;
+  min-width: 100px; 
+
+  &:hover,
+  &:focus {
+    border-color:#1a75ff;
+    outline: none;
+  }
+`;
+
+const SortButton = styled.button`
+  background-color: #333333;
+  border: 1px solid #ccc;
+  color: #fff;
+  font-size: 14px; 
+  padding: 4px 2px; 
+  margin-top: 15px;
+  cursor: pointer;
+  border-radius: 5px;
+  transition: all 0.3s ease-in-out;
+  display: flex;
+  align-items: center;
+  justify-content: center; 
+  min-width: 120px; 
+  
+  &:hover,
+  &:focus {
+    border-color: #1a75ff;
+    outline: none;
+  }
+
+  span {
+    margin-right: 2px;
+    margin-left: 6px;
+  }
+
+  svg {
+    font-size: 20px;
+    color: #fff;
+  }
+`;
 
 function DeviceManagement() {
   const navigate = useNavigate();
@@ -211,7 +278,6 @@ function DeviceManagement() {
       fetchSoundLevels(device.deviceId); 
     }
   };
-  
 
   const fetchSoundLevels = async (deviceId) => {
     setIsFetching(true);
@@ -240,7 +306,73 @@ function DeviceManagement() {
   const handleChange = (e, field) => {
     setEditedData({ ...editedData, [field]: e.target.value });
   };
+
+  const [sortByLevelOrder, setSortByLevelOrder] = useState("asc"); 
+  const toggleSortByLevelOrder = () => {
+    setSortByLevelOrder((prevOrder) => (prevOrder === "asc" ? "desc" : "asc"));
+  };
   
+
+
+  const [timeFilter, setTimeFilter] = useState("1 hour");
+  const [dangerFilter, setDangerFilter] = useState("all"); 
+  const filterSoundLevels = useMemo(() => {
+    const now = new Date();
+    let timeRange = 0;
+  
+    // Set time range based on the time filter
+    switch (timeFilter) {
+      case "1 hour":
+        timeRange = 1;
+        break;
+      case "3 hours":
+        timeRange = 3;
+        break;
+      case "7 hours":
+        timeRange = 7;
+        break;
+      case "12 hours":
+        timeRange = 12;
+        break;
+      case "1 day":
+        timeRange = 24;
+        break;
+      case "3 days":
+        timeRange = 72;
+        break;
+      case "7 days":
+        timeRange = 168;
+        break;
+      default:
+        timeRange = 1;
+    }
+  
+    // Filter sound levels by time and danger filter
+    const filteredLevels = soundLevelsMemoized.filter((sound) => {
+      const soundDate = new Date(sound.date);
+      const diffHours = (now - soundDate) / (1000 * 60 * 60);
+  
+      // Apply time filter
+      const matchesTimeFilter = diffHours <= timeRange;
+  
+      // Apply danger filter
+      const matchesDangerFilter = dangerFilter === "all" || sound.level > 85;
+  
+      return matchesTimeFilter && matchesDangerFilter;
+    });
+  
+    // Sort by level (ascending or descending based on the order)
+    return filteredLevels.sort((a, b) => {
+      if (sortByLevelOrder === "asc") {
+        return a.level - b.level;
+      } else {
+        return b.level - a.level;
+      }
+    });
+  
+  }, [soundLevelsMemoized, timeFilter, dangerFilter, sortByLevelOrder]);  // Include `sortByLevelOrder` in dependencies
+  
+
   const handleSave = async (soundId) => {
     if (!editedData.level || !editedData.result) {
       alert("Please enter both sound level and result.");
@@ -322,114 +454,124 @@ function DeviceManagement() {
                     {expandedRow === device.id && (
                       <tr style={{ backgroundColor: '#222222' }}> 
                         <Td colSpan="6" style={{ padding: '10px' }}>
+                          
                           <div>
-                            <h2>Device Info</h2>
-                            <p><strong>Name:</strong> {device.deviceName}</p>
-                            <p><strong>ID:</strong> {device.deviceId}</p>
-                            <p><strong>Location:</strong> {device.location}</p>
-                            <p><strong>Status:</strong> {device.status}</p>
-
                             <h2>Sound Levels</h2>
+                            <FiltersWrapper>
+                            <div>
+                            <FilterContainer>
+                            <FilterSelect
+                              value={dangerFilter}
+                              onChange={(e) => setDangerFilter(e.target.value)}
+                              style={{ marginLeft: "10px", padding: "5px" }}
+                            >
+                              <option value="all">Show All Sounds</option>
+                              <option value="danger">Show Only Danger Sounds</option>
+                            </FilterSelect>
+                            </FilterContainer>
+                          </div>
+                            <div>
+                            <FilterContainer>
+                            <FilterSelect
+                              value={timeFilter}
+                              onChange={(e) => setTimeFilter(e.target.value)}
+                              style={{ marginLeft: "10px", padding: "5px" }}
+                            >
+                              <option value="1 hour">Last 1 Hour</option>
+                              <option value="3 hours">Last 3 Hours</option>
+                              <option value="7 hours">Last 7 Hours</option>
+                              <option value="12 hours">Last 12 Hours</option>
+                              <option value="1 day">Last 1 Day</option>
+                              <option value="3 days">Last 3 Days</option>
+                              <option value="7 days">Last 7 Days</option>
+                            </FilterSelect>
+                            </FilterContainer>
+                          </div>
+                          <div>
+                            <FilterContainer>
+                            <SortButton onClick={toggleSortByLevelOrder}>
+                              <span>Sort by Level</span>
+                              {sortByLevelOrder === "asc" ? (
+                                <ArrowUpward />
+                              ) : (
+                                <ArrowDownward />
+                              )}
+                            </SortButton>
+                            </FilterContainer>
+                          </div>
+                          </FiltersWrapper>
                             {isFetching ? (
                               <p>Loading sound levels...</p>
                             ) : soundLevelsMemoized.length === 0 ? (
                               <p>No sound data available</p>
                             ) : (
                               <ScrollableContent>
-                                <ul style={{ padding: 0, listStyle: "none" }}>
-          {soundLevelsMemoized.map((sound, index) => (
-            <li 
-              key={index}
-              style={{
-                color: sound.color,
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "center",  
-                gap: "10px",  
-                padding: "2px",
-                fontSize: "16px",
-                width: "100%", 
-                textAlign: "center", 
-              }}
-            >
-              {sound.level > 85 ? (
-                <>
-                  {/* Show dropdown when editing */}
-                  {editingIndex === index ? (
-                    <div style={{ display: "flex", gap: "10px", alignItems: "center" }}>
-                      {/* Display Date and dB */}
-                      <span>{sound.formattedDate} - {sound.level} dB - </span>
-
-                      {/* Dropdown for classification result */}
-                      <select
-                        value={editedData.result || sound.result} // Use editedData if available, else the original result
-                        onChange={(e) => handleChange(e, "result")}
-                        style={{ width: "120px", padding: "5px" }}
-                      >
-                        <option value="Engine">Engine</option>
-                        <option value="Airplane">Airplane</option>
-                        <option value="Car">Car</option>
-                        <option value="Train">Train</option>
-                        <option value="Car Horn">Car Horn</option>
-                        <option value="Chainsaw">Chainsaw</option>
-                        <option value="Drilling">Drilling</option>
-                        <option value="Handsaw">Handsaw</option>
-                        <option value="Jackhammer">Jackhammer</option>
-                        <option value="Street Music">Street Music</option>
-                        <option value="Other Sounds">Other Sounds</option>
-                      </select>
-
-                      {/* Save button */}
-                      <button 
-                        onClick={() => handleSave(sound.id)} 
-                        disabled={saving}
-                        style={{
-                          backgroundColor: "#28a745", 
-                          color: "white",
-                          padding: "6px 10px", 
-                          border: "none",
-                          borderRadius: "5px", 
-                          cursor: "pointer",
-                          fontSize: "14px",
-                          transition: "0.3s ease-in-out", 
-                        }}
-                      >
-                        {saving ? "Saving..." : "Save"}
-                      </button>
-                    </div>
-                  ) : (
-                    <>
-                      {/* Display Date and dB with clickable classification */}
-                      <span>{sound.formattedDate} - {sound.level} dB - </span>
-                      <span 
-                        onClick={() => handleEditClick(index, sound)} 
-                        style={{ cursor: "pointer", textDecoration: "underline" }}
-                      >
-                        {sound.result}
-                      </span>
-                      <SoundIcon 
-                        onClick={() => {
-                          if (sound.sample) {
-                            new Audio(sound.sample).play();
-                          } else {
-                            alert("No Audio Available");
-                          }
-                        }}
-                      >
-                        🔊
-                      </SoundIcon>
-                    </>
-                  )}
-                </>
-              ) : (
-                <span>
-                  {sound.formattedDate} - {sound.level} dB
-                </span>
-              )}
-            </li>
-          ))}
-        </ul>
-                              </ScrollableContent>
+                              <ul style={{ padding: 0, listStyle: "none" }}>
+                                {filterSoundLevels.map((sound, index) => (
+                                  <li 
+                                    key={index}
+                                    style={{
+                                      color: sound.color,
+                                      display: "flex",
+                                      alignItems: "center",
+                                      justifyContent: "center",  
+                                      gap: "10px",  
+                                      padding: "2px",
+                                      fontSize: "16px",
+                                      width: "100%", 
+                                      textAlign: "center", 
+                                    }}
+                                  >
+                                    {sound.level > 85 ? (
+                                      <>
+                                        {editingIndex === index ? (
+                                          <div style={{ display: "flex", gap: "10px", alignItems: "center" }}>
+                                            <span>{sound.formattedDate} - {sound.level} dB - </span>
+                                            <select
+                                              value={editedData.result || sound.result}
+                                              onChange={(e) => handleChange(e, "result")}
+                                              style={{ width: "120px", padding: "5px" }}
+                                            >
+                                              <option value="Engine">Engine</option>
+                                              <option value="Airplane">Airplane</option>
+                                              <option value="Car">Car</option>
+                                              <option value="Train">Train</option>
+                                              <option value="Car Horn">Car Horn</option>
+                                              <option value="Chainsaw">Chainsaw</option>
+                                              <option value="Drilling">Drilling</option>
+                                              <option value="Handsaw">Handsaw</option>
+                                              <option value="Jackhammer">Jackhammer</option>
+                                              <option value="Street Music">Street Music</option>
+                                              <option value="Other Sounds">Other Sounds</option>
+                                            </select>
+                                            <button 
+                                              onClick={() => handleSave(sound.id)} 
+                                              disabled={saving} 
+                                              style={{ backgroundColor: "#28a745", color: "white", padding: "6px 10px", border: "none", borderRadius: "5px", cursor: "pointer", fontSize: "14px", transition: "0.3s ease-in-out" }}
+                                            >
+                                              {saving ? "Saving..." : "Save"}
+                                            </button>
+                                          </div>
+                                        ) : (
+                                          <>
+                                            <span>{sound.formattedDate} - {sound.level} dB - </span>
+                                            <span onClick={() => handleEditClick(index, sound)} style={{ cursor: "pointer", textDecoration: "underline" }}>
+                                              {sound.result}
+                                            </span>
+                                            <SoundIcon onClick={() => sound.sample ? new Audio(sound.sample).play() : alert("No Audio Available")}>
+                                              🔊
+                                            </SoundIcon>
+                                          </>
+                                        )}
+                                      </>
+                                    ) : (
+                                      <span>{sound.formattedDate} - {sound.level} dB</span>
+                                    )}
+                                  </li>
+                                ))}
+                              </ul>
+                            </ScrollableContent>
+                            
                             )}
                           </div>
                         </Td>
